30f57a32fa1dd68200be55faf07bfb90b6a721b8
ifetiwa <43348467+ifetiwa@users.noreply.github.com>
2023-02-22 12:08:19 +0100

    Add files via upload

---
+++ lib/widgets/conversationList.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/chatDetailPage.dart';
+
+class ConversationList extends StatefulWidget{
+  String text;
+  String secondaryText;
+  String image;
+  String time;
+  bool isMessageRead;
+  ConversationList({@required this.text,@required this.secondaryText,@required this.image,@required this.time,@required this.isMessageRead});
+  @override
+  _ConversationListState createState() => _ConversationListState();
+}
+
+class _ConversationListState extends State<ConversationList> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return ChatDetailPage();
+        }));
+      },
+      child: Container(
+        padding: EdgeInsets.only(left: 16,right: 16,top: 10,bottom: 10),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(widget.image),
+                    maxRadius: 30,
+                  ),
+                  SizedBox(width: 16,),
+                  Expanded(
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(widget.text, style: TextStyle(fontSize: 16),),
+                          SizedBox(height: 6,),
+                          Text(widget.secondaryText,style: TextStyle(fontSize: 13,color: Colors.grey.shade600, fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(widget.time,style: TextStyle(fontSize: 12,fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),),
+          ],
+        ),
+      ),
+    );
+  }
+}
